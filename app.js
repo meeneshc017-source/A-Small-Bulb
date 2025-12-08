326c65818614f2178cfd58ca35efefe245a3a9b6
Meenesh Chaudhary <meeneshc017@gmail.com>
2025-12-08 21:34:20 +0530

    added button function

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
  
  let bulb=document.querySelector("#bulb")
- setInterval(()=>{
+
+ const bulbb=() =>{
+     setInterval(()=>{
      bulb.classList.toggle("bulb")
 },200)
+ }
 
+let btn=document.querySelector("#btn")
+btn.addEventListener("click",()=>{
+    bulbb()
+})
 
+btn.removeEventListener("click",()=>{
+    bulbb()
+});
